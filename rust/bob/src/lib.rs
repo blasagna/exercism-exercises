aa806d08f1c442ec28168c7a63cbfe03af2431fb
blasagna <bdimaiolo@gmail.com>
2017-11-02 20:17:40 -0400

    first working bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -6,20 +6,37 @@ enum ReplyTo {
 }
 
 pub fn reply(message: &str) -> &str {
-    //todo: continue with remaining test failures
     let mut reply: ReplyTo = ReplyTo::Other;
     let msg_processed = message.trim();
     if msg_processed.len() == 0 {
         reply = ReplyTo::Statement;
-    } else if msg_processed.ends_with('!') {
-        reply = ReplyTo::Yell;
-    } else if msg_processed.ends_with('?') {
-        reply = ReplyTo::Question;
-        if msg_processed == msg_processed.to_uppercase() {
-            reply = ReplyTo::Yell;
+    } else {
+        // determine if any letters present
+        let mut any_letters = false;
+        for char in msg_processed.chars() {
+            if char.is_alphabetic() {
+                any_letters = true;
+            }
         }
-    } else if msg_processed == msg_processed.to_uppercase() {
-        reply = ReplyTo::Yell;
+
+        if any_letters {
+            if msg_processed.ends_with('!') && msg_processed == msg_processed.to_uppercase() {
+                reply = ReplyTo::Yell;
+            } else if msg_processed.ends_with('?') {
+                reply = ReplyTo::Question;
+                if msg_processed == msg_processed.to_uppercase() {
+                    reply = ReplyTo::Yell;
+                }
+            } else if msg_processed == msg_processed.to_uppercase() {
+                reply = ReplyTo::Yell;
+            }
+        } else {
+            if msg_processed.ends_with('?') {
+               reply = ReplyTo::Question;
+            } else if msg_processed.ends_with('!') {
+                reply = ReplyTo::Yell;
+            }
+        }
     }
 
 
