0e745d3421d728d9d8bb880cc19d6aa76ef0ff39
blasagna <bdimaiolo@gmail.com>
2017-11-04 14:30:50 -0400

    python is_isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,11 @@
 def is_isogram(string):
-    pass
+    seen = 0
+    norm_string = string.lower()
+    for c in norm_string:
+        if c in ('-', ' '):
+            continue
+        mask = 1 << ord(c)
+        if seen & mask:
+            return False
+        seen |= mask
+    return True
