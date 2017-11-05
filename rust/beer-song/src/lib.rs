0f9ee124a839d9ff59b6a0688ba916a3d4680ce5
blasagna <bdimaiolo@gmail.com>
2017-11-05 11:43:59 -0500

    beer song with string format

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,28 +1,16 @@
 pub fn verse(n: i32) -> String {
-  let mut verse = String::from("");
-
   let amount = n.to_string();
-
   let n_str = if n > 0 {
     &amount
   } else {
     "No more"
   };
-  verse.push_str(n_str);
 
   let n_sing_plural = if n != 1 {
     "bottles"
   } else {
     "bottle"
   };
-  verse.push_str(" ");
-  verse.push_str(n_sing_plural);
-  verse.push_str(" of beer on the wall, ");
-
-  verse.push_str(&(n_str.to_lowercase()));
-  verse.push_str(" ");
-  verse.push_str(n_sing_plural);
-  verse.push_str(" of beer.\n");
 
   let m = n - 1;
   let decr_amount = m.to_string();
@@ -38,26 +26,20 @@ pub fn verse(n: i32) -> String {
     "it"
   };
 
-  if m > -1 {
-    verse.push_str("Take ");
-    verse.push_str(take_down);
-    verse.push_str(" down and pass it around, ");
-
-    let m_sing_plural = if m != 1 {
-      "bottles"
-    } else {
-      "bottle"
-    };
-
-    verse.push_str(m_str);
-    verse.push_str(" ");
-    verse.push_str(m_sing_plural);
-    verse.push_str(" of beer on the wall.\n");
+  let m_sing_plural = if m != 1 {
+    "bottles"
   } else {
-    verse.push_str("Go to the store and buy some more, 99 bottles of beer on the wall.\n");
-  }
+    "bottle"
+  };
 
-  verse
+  let first = format!("{} {} of beer on the wall, {} {} of beer.", n_str, n_sing_plural, n_str.to_lowercase(), n_sing_plural);
+  let second = if m >= 0 {
+    format!("Take {} down and pass it around, {} {} of beer on the wall.", take_down, m_str, m_sing_plural)
+  } else {
+    "Go to the store and buy some more, 99 bottles of beer on the wall.".to_string()
+  };
+
+  format!("{}\n{}\n", first, second)
 }
 
 pub fn sing(start: i32, end: i32) -> String {
